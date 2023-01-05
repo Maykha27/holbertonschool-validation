29c8a38ebb5ae4570cba3a45de25472b310a261e
Maykha27 <5144@holbertonschool.com>
2023-01-05 21:16:00 +0100

    module3_task0/

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-#!/bin/bash
 apt-get update && apt-get install -y wget make
 wget https://github.com/gohugoio/hugo/releases/download/v0.88.1/hugo_0.88.1_Linux-64bit.deb
 dpkg -i hugo_0.88.1_Linux-64bit.deb
