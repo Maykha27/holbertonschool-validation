1e03a27345fce7b6560a1f5dba25871884e993b7
Maykha27 <5144@holbertonschool.com>
2023-01-13 15:07:43 +0100

    kjg

--- module3_task3/setup.sh
+++ module3_task3/setup.sh
@@ -4,4 +4,3 @@ apt-get install curl -y
 curl -L install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugoversion.deb
 apt-get install ./install_hugo.deb
 npm install -g markdownlint-cli
-rm ./install_hugo.deb
